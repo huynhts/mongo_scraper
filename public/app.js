c2a10ae4c70343b2cd04baf214eeae90cde7de75
winTS <huynhts@gmail.com>
2019-09-29 22:27:56 -0700

    add buttons to view/edit/save notes

--- public/app.js
+++ public/app.js
@@ -12,24 +12,47 @@ $.getJSON("/articles", function(data) {
       postLink = artLink;
     }
     
-    console.log(postLink);
-
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].summary + "<br />" + "<a href='" + postLink + "' target='#'>" + "Read Article Here" + "</a>" + "</p>");
+    $("#articles").append(
+      "<p'>" 
+      + data[i].title 
+      + "<br />" 
+      + data[i].summary 
+      + "<br />" 
+      + "<a href='" + postLink + "' target='#'>" + "Read Article Here " + "</a>" 
+      + "<button data-id='" + data[i]._id + "' id='addNote'> Add Notes </button>"
+      + "<button note-id='" + data[i]._id + "' id='viewNote'> View Notes </button>"
+      + "<button article-id='" + data[i]._id + "' id='saveArt'> Save Article </button>"
+      + "</p>");
   }
 });
 
 
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
-  // Empty the notes from the note section
+// Whenever someone clicks add note button
+$(document).on("click", "#addNote", function() {
+      // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
   var thisId = $(this).attr("data-id");
 
+  noteTaking(thisId);
+});
+
+// Whenever someone clicks edit note button
+$(document).on("click", "#editNote", function() {
+      // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  noteTaking(thisId);
+});
+
+var noteTaking = function (noteId) {
+
   // Now make an ajax call for the Article
   $.ajax({
     method: "GET",
-    url: "/articles/" + thisId
+    url: "/articles/" + noteId
   })
     // With that done, add the note information to the page
     .then(function(data) {
@@ -43,15 +66,16 @@ $(document).on("click", "p", function() {
       // A button to submit a new note, with the id of the article saved to it
       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
-});
+
+    // If there's a note in the article
+    if (data.note) {
+      // Place the title of the note in the title input
+      $("#titleinput").val(data.note.title);
+      // Place the body of the note in the body textarea
+      $("#bodyinput").val(data.note.body);
+    }
+  });
+};
 
 // When you click the savenote button
 $(document).on("click", "#savenote", function() {
@@ -80,4 +104,38 @@ $(document).on("click", "#savenote", function() {
   // Also, remove the values entered in the input and textarea for note entry
   $("#titleinput").val("");
   $("#bodyinput").val("");
+});
+
+//when you click view note
+$(document).on("click", "#viewNote", function() {
+  // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("note-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      console.log(data);
+      
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#notes").append("<h5> Note Title: " + data.note.title + "</h5>");
+      // A textarea to add a new note body
+      $("#notes").append("<p>" + data.note.body + "</p>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button data-id='" + data._id + "' id='editNote'> Edit Notes</button>");
+      // A button to delete note
+      $("#notes").append("<button data-id='" + data._id + "' id='deleteNote'>Delete Note</button>");
+
+    });
+});
+
+// Whenever someone clicks add note button
+$(document).on("click", "#saveArt", function() {
+  //$set save status to true and turn button green
 });
